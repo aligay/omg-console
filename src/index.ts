3f54bc66e65422de70f48d9ccc998bb881feaca5
浩知 <haozhi.lyj@alibaba-inc.com>
2019-02-21 11:10:34 +0800

    增加类型推断

--- src/index.ts
+++ src/index.ts
@@ -4,39 +4,136 @@ import { getOriginalPosition } from './sourcemap'
 const isString = s => typeof s === 'string'
 const isNumber = n => typeof n === 'number'
 const isBoolean = b => typeof b === 'boolean'
-
 const pad = (s, n = 2) => (s + '').padStart(n, '0')
+
 interface IOptions {
   name: string
 }
 
-export default class Logger {
-  public log
-  public debug
-  public info
-  public dirxml
-  public warn
-  public error
-  public dir
-  public time
-  public timeEnd
-  public timeLog
-  public trace
-  public assert
-  public clear
-  public count
-  public countReset
-  public group
-  public groupCollapsed
-  public groupEnd
-  public table
-  public Console
-  public profile
-  public profileEnd
-  public timeStamp
-  public context
+export default class Console {
+  /**
+   * A simple assertion test that verifies whether `value` is truthy.
+   * If it is not, an `AssertionError` is thrown.
+   * If provided, the error `message` is formatted using `util.format()` and used as the error message.
+   */
+  assert!: (value: any, message?: string, ...optionalParams: any[]) => void
+  /**
+   * When `stdout` is a TTY, calling `console.clear()` will attempt to clear the TTY.
+   * When `stdout` is not a TTY, this method does nothing.
+   */
+  clear!: () => void
+  /**
+   * Maintains an internal counter specific to `label` and outputs to `stdout` the number of times `console.count()` has been called with the given `label`.
+   */
+  count!: (label?: string) => void
+  /**
+   * Resets the internal counter specific to `label`.
+   */
+  countReset!: (label?: string) => void
+  /**
+   * The `console.debug()` function is an alias for {@link console.log()}.
+   */
+  debug!: (message?: any, ...optionalParams: any[]) => void
+  /**
+   * Uses {@link util.inspect()} on `obj` and prints the resulting string to `stdout`.
+   * This function bypasses any custom `inspect()` function defined on `obj`.
+   */
+  dir!: (obj: any, options?: NodeJS.InspectOptions) => void
+  /**
+   * This method calls {@link console.log()} passing it the arguments received. Please note that this method does not produce any XML formatting
+   */
+  dirxml!: (...data: any[]) => void
+  /**
+   * Prints to `stderr` with newline.
+   */
+  error!: (message?: any, ...optionalParams: any[]) => void
+  /**
+   * Increases indentation of subsequent lines by two spaces.
+   * If one or more `label`s are provided, those are printed first without the additional indentation.
+   */
+  group!: (...label: any[]) => void
+  /**
+   * The `console.groupCollapsed()` function is an alias for {@link console.group()}.
+   */
+  groupCollapsed!: () => void
+  /**
+   * Decreases indentation of subsequent lines by two spaces.
+   */
+  groupEnd!: () => void
+  /**
+   * The {@link console.info()} function is an alias for {@link console.log()}.
+   */
+  info!: (message?: any, ...optionalParams: any[]) => void
+  /**
+   * Prints to `stdout` with newline.
+   */
+  log!: (message?: any, ...optionalParams: any[]) => void
+  /**
+   * This method does not display anything unless used in the inspector.
+   *  Prints to `stdout` the array `array` formatted as a table.
+   */
+  table!: (tabularData: any, properties?: string[]) => void
+  /**
+   * Starts a timer that can be used to compute the duration of an operation. Timers are identified by a unique `label`.
+   */
+  time!: (label?: string) => void
+  /**
+   * Stops a timer that was previously started by calling {@link console.time()} and prints the result to `stdout`.
+   */
+  timeEnd!: (label?: string) => void
+  /**
+   * For a timer that was previously started by calling {@link console.time()}, prints the elapsed time and other `data` arguments to `stdout`.
+   */
+  timeLog!: (label?: string, ...data: any[]) => void
+  /**
+   * Prints to `stderr` the string 'Trace :', followed by the {@link util.format()} formatted message and stack trace to the current position in the code.
+   */
+  trace!: (message?: any, ...optionalParams: any[]) => void
+  /**
+   * The {@link console.warn()} function is an alias for {@link console.error()}.
+   */
+  warn!: (message?: any, ...optionalParams: any[]) => void
+
+  // --- Inspector mode only ---
+  /**
+   * This method does not display anything unless used in the inspector.
+   *  The console.markTimeline() method is the deprecated form of console.timeStamp().
+   *
+   * @deprecated Use console.timeStamp() instead.
+   */
+  markTimeline!: (label?: string) => void
+  /**
+   * This method does not display anything unless used in the inspector.
+   *  Starts a JavaScript CPU profile with an optional label.
+   */
+  profile!: (label?: string) => void
+  /**
+   * This method does not display anything unless used in the inspector.
+   *  Stops the current JavaScript CPU profiling session if one has been started and prints the report to the Profiles panel of the inspector.
+   */
+  profileEnd!: (label?: string) => void
+  /**
+   * This method does not display anything unless used in the inspector.
+   *  Adds an event with the label `label` to the Timeline panel of the inspector.
+   */
+  timeStamp!: (label?: string) => void
+  /**
+   * This method does not display anything unless used in the inspector.
+   *  The console.timeline() method is the deprecated form of console.time().
+   *
+   * @deprecated Use console.time() instead.
+   */
+  timeline!: (label?: string) => void
+  /**
+   * This method does not display anything unless used in the inspector.
+   *  The console.timelineEnd() method is the deprecated form of console.timeEnd().
+   *
+   * @deprecated Use console.timeEnd() instead.
+   */
+  timelineEnd!: (label?: string) => void
+
   private mapping = {
-    log: ['white', ''],
+    log: ['white'],
     info: ['green'],
     error: ['red'],
     warn: ['yellow'],
@@ -83,11 +180,14 @@ export default class Logger {
 
   private _getNow (): string {
     const d = new Date()
+    const yyyy = d.getFullYear()
+    const MM = pad(d.getMonth() + 1)
+    const dd = pad(d.getDate())
     const HH = pad(d.getHours())
     const mm = pad(d.getMinutes())
     const ss = pad(d.getSeconds())
     const ms = pad(d.getMilliseconds(), 3)
-    return `${HH}:${mm}:${ss}.${ms}`
+    return `${yyyy}-${MM}-${dd} ${HH}:${mm}:${ss}.${ms}`
   }
 
   /**
@@ -95,7 +195,6 @@ export default class Logger {
    */
   private _getInfo (): Promise<{
     pathname: string
-    filename: string
     line: number
     column: number
   }> {
@@ -110,20 +209,17 @@ export default class Logger {
       info = matched[1]
     }
     let [pathname, line, column] = info.split(':')
-    let filename = path.relative(__dirname, pathname)
     line = +line
     column = +column
     return new Promise(resolve => {
       getOriginalPosition({ codeUrl: pathname, line, column }).then(rawInfo => {
         if (rawInfo && rawInfo.source) {
           pathname = path.resolve(pathname, '../', rawInfo.source)
-          filename = rawInfo.source
           line = rawInfo.line
           column = rawInfo.column
         }
         resolve({
           pathname,
-          filename,
           line,
           column
         })
